093734fcd89ddcfbcf3b22f79f46414f57683c9a
Binaryinject <wbn1883@vip.qq.com>
2025-02-09 10:43:35 +0800

    update

---
+++ src/AsterCleaner/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+using Microsoft.Win32;
+
+
+if (Directory.Exists(@"C:\ProgramData\ASTER Control")) Directory.Delete(@"C:\ProgramData\ASTER Control", true);
+
+const string keyPath = @"SOFTWARE\IBIK\ASTER\asterctx";
+var key = Registry.LocalMachine.OpenSubKey(keyPath, true);
+if (key != null) Registry.LocalMachine.DeleteSubKeyTree(keyPath);
+
+Process.Start("SecHex-Spoofy/SecHex-GUI.exe");
